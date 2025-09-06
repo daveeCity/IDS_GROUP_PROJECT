4a9a285de856c67c41e2f7fe72976086442279e6
dave <davudsufa394@gmail.com>
2025-09-06 18:10:04 +0200

    added IdGenerator

---
+++ piattaformaFilieraAgricola/src/main/java/it/unicam/cs/filieraagricola/util/IdGenerator.java
@@ -0,0 +1,8 @@
+package main.java.it.unicam.cs.filieraagricola.util;
+public class IdGenerator {
+    private static long counter = 1;
+
+    public static synchronized long generateId() {
+        return counter++;
+    }
+}
